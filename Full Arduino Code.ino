8b819a236fb1efafe973fc6aaba57c5e2c239496
BrickDragon1 <129925218+BrickDragon1@users.noreply.github.com>
2023-05-07 21:28:50 +1000

    Update Full Arduino Code.ino

--- Full Arduino Code.ino
+++ Full Arduino Code.ino
@@ -1,26 +1,42 @@
-//Seven Segment Display
-int a = 11; //top
-int f = 9; //top left
-int b = 12; //top right
-int g = 10; //middle
-int c= 7; //bottom right
-int e = 6; //bottom left
-int d = 2; //bottom
+//Ultrasonic sensor
+#include <NewPing.h>  
 
-//Ultrasonic Sensor
-#include <NewPing.h>
-#define TRIGGER_PIN  12  
-#define ECHO_PIN     11  
-#define MAX_DISTANCE 11 
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+#define TRIGGER_PIN  A1  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     A0  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 11
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+
+//LEDs
+const int red = 3;
+const int green = 2;
+
+//Seven Segment Display
+int a = 10;
+int b = 11;
+int c = 13;
+int d = 9;
+int e = 12;
+int f = 7;
+int g = 8 ;
+
+//Micro-servo
+#include <Servo.h>
+Servo myservo;
 
 //Liquid Crystal Display
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+LiquidCrystal_I2C lcd(0X27,16,2);
 
-//Micro-Servo
-
-void setup(){
-//Ultrasonic Sensor
+void setup() {
+//Ultrasonic
 Serial.begin(9600);
+
+//LEDs
+pinMode(red, OUTPUT);
+pinMode(green, OUTPUT);
+digitalWrite(red, LOW);
+digitalWrite(green, HIGH);
 
 //Seven Segment Display
 pinMode(a, OUTPUT);
@@ -32,35 +48,52 @@ pinMode(f, OUTPUT);
 pinMode(g, OUTPUT);
   
 //Micro-servo
+ myservo.attach(A3);
+ myservo.write(80);
+  
+ //Liquid Crystal Display
+  lcd.begin();
+  lcd.backlight();
 }
-//Seven Segment Display
 int pins[7] = {a, b, c, d, e, f, g};
+
 void displayNumber(int *number) {
   for (int i = 0; i < 7; i++) {
     digitalWrite(pins[i], number[i]);
   }
 }
 
-void Ultrasonic(){
-  delay(50);                   
-  Serial.print("Ping: ");
-  Serial.print(sonar.ping_cm());
-  Serial.println("cm");
+void distance(){
+delay(50);                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+Serial.print("Ping: ");
+Serial.print(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
+Serial.println("cm"); 
 }
- 
-void loop(){
-//Seven Segment Display
+
+void led_change(){
 int numberCount = 2;
- 
+
 int numbers[numberCount][7] = {
   {1, 1, 1, 1, 1, 1, 0}, // 0
   {0, 1, 1, 0, 0, 0, 0}, // 1
 };
-
-Ultrasonic();
- 
-for (int i = 0; i < numberCount; i++) {
-  displayNumber(numbers[i]);
-  delay(1000);
+  
+if(sonar.ping_cm() < 10){
+digitalWrite(red, HIGH);
+digitalWrite(green, LOW);
+displayNumber(numbers[0]);
+}else{
+ digitalWrite(red, LOW);
+ digitalWrite(green, HIGH);
+ displayNumber(numbers[1]);
+}  
 }
+
+void loop() {
+delay(50);                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+Serial.print("Ping: ");
+Serial.print(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
+Serial.println("cm"); 
+delay(1000);
+led_change();
 }
